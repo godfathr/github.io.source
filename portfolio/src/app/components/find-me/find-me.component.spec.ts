8eb1501889aa083a4c6d7bee89c5aa6a76fbc933
godfathr <mikal.k.grant@outlook.com>
2021-06-10 17:46:35 -0600

    Upgraded version of Angular to 12.0.4

--- portfolio/src/app/components/find-me/find-me.component.spec.ts
+++ portfolio/src/app/components/find-me/find-me.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { FindMeComponent } from './find-me.component';
 
@@ -6,7 +6,7 @@ describe('FindMeComponent', () => {
   let component: FindMeComponent;
   let fixture: ComponentFixture<FindMeComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ FindMeComponent ]
     })
--- portfolio/src/app/components/know-me/know-me.component.spec.ts
+++ portfolio/src/app/components/know-me/know-me.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { KnowMeComponent } from './know-me.component';
 
@@ -6,7 +6,7 @@ describe('KnowMeComponent', () => {
   let component: KnowMeComponent;
   let fixture: ComponentFixture<KnowMeComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ KnowMeComponent ]
     })
