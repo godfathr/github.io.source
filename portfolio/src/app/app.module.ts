37f8e642e666f38c6db8715157cf7511403ffb3d
Mikal Grant <17180852+godfathr@users.noreply.github.com>
2022-04-24 15:17:54 -0600

    Rearranged content.

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -1,11 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
 import { SnippetsComponent } from './components/snippets/snippets.component';
-import { KnowMeComponent } from './components/know-me/know-me.component';
 import { FindMeComponent } from './components/find-me/find-me.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { FooterComponent } from './components/footer/footer.component';
@@ -15,9 +12,7 @@ import { CommonModule } from '@angular/common';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     SnippetsComponent,
-    KnowMeComponent,
     FindMeComponent,
     NavigationComponent,
     FooterComponent
--- portfolio/src/app/components/snippets/snippets.component.ts
+++ portfolio/src/app/components/snippets/snippets.component.ts
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SnippetsComponent implements OnInit {
 
+  public displayPassion: boolean = false;
+  public displayResponsive: boolean = false;
+  displaySecurity: boolean;
+  displayVariety: boolean;
+  displayDream: boolean;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  public togglePassion() {
+    this.displayPassion = !this.displayPassion;
+  }
+  public toggleResponsive() {
+    this.displayResponsive = !this.displayResponsive;
+  }
+  public toggleSecurity() {
+    this.displaySecurity = !this.displaySecurity;
+  }
+  public toggleVariety() {
+    this.displayVariety = !this.displayVariety;
+  }
+  public toggleDream(){
+    this.displayDream = !this.displayDream;
+  }
 }
