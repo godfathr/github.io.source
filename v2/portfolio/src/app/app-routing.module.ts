0b3a5e284f69072f23138ea4a6f63630e0d81c26
Mikal Grant <17180852+godfathr@users.noreply.github.com>
2022-05-09 12:47:15 -0600

    Added component placeholders for content and implemented routing.

--- v2/portfolio/src/app/app-routing.module.ts
+++ v2/portfolio/src/app/app-routing.module.ts
@@ -1,10 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { AppComponent } from './app.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { FeaturedComponent } from './featured/featured.component';
+import { FindMeComponent } from './find-me/find-me.component';
+import { HomeComponent } from './home/home.component';
+import { IntroComponent } from './intro/intro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'about', component: AboutComponent },
+  { path: 'experience', component: ExperienceComponent },
+  { path: 'featured', component: FeaturedComponent },
+  { path: 'find-me', component: FindMeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'intro', component: IntroComponent },
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  declarations: [
+  ],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppRoutingModule { }
--- v2/portfolio/src/app/app.module.ts
+++ v2/portfolio/src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IntroComponent } from './intro/intro.component';
+import { HomeComponent } from './home/home.component';
+import { FeaturedComponent } from './featured/featured.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { AboutComponent } from './about/about.component';
+import { FindMeComponent } from './find-me/find-me.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IntroComponent
+    IntroComponent,
+    HomeComponent,
+    FeaturedComponent,
+    ExperienceComponent,
+    AboutComponent,
+    FindMeComponent
   ],
   imports: [
     BrowserModule,
