fbc30fe6c94e1588e6622f45a9f61322dba218bd
Mikal Grant <17180852+godfathr@users.noreply.github.com>
2022-06-27 10:36:23 -0600

    update metadata assets and small tweaks

--- portfolio/src/app/components/find-me/find-me.component.ts
+++ portfolio/src/app/components/find-me/find-me.component.ts
@@ -18,66 +18,68 @@ export class FindMeComponent implements OnInit {
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    this.getProfile();
-    this.getFollowers().subscribe(data => this.followers = data);
+    // this.getProfile();
+    // this.getFollowers().subscribe(data => this.followers = data);
   }
 
-  getProfile() {
-    this.http.get('https://api.github.com/users/godfathr')
-      .subscribe((response) => {
-        this.response = response;
-      });
-  }
+  //   getProfile() {
+  //     this.http.get('https://api.github.com/users/godfathr')
+  //       .subscribe((response) => {
+  //         this.response = response;
+  //       });
+  //   }
 
-  getFollowers(): Observable<IFollower[]> {
-    return this.http
-      .get<IFollower[]>('https://api.github.com/users/godfathr/followers');
-    // .map(result => result.followers);
-    //   followers => {
-    //   return followers.json().data.map((elt) => {
-    //     return new Follower[](elt);
-    //   });
-    // })
-    // .subscribe((followers) => {
+  //   getFollowers(): Observable<IFollower[]> {
+  //     return this.http
+  //       .get<IFollower[]>('https://api.github.com/users/godfathr/followers');
+  //     // .map(result => result.followers);
+  //     //   followers => {
+  //     //   return followers.json().data.map((elt) => {
+  //     //     return new Follower[](elt);
+  //     //   });
+  //     // })
+  //     // .subscribe((followers) => {
 
-    // });
-  }
+  //     // });
+  //   }
 
-  private handleError(error: HttpErrorResponse) {
-    if (error.error instanceof ErrorEvent) {
-      // A client-side or network error occurred. Handle it accordingly.
-      console.error('An error occurred:', error.error.message);
-    } else {
-      // The backend returned an unsuccessful response code.
-      // The response body may contain clues as to what went wrong,
-      console.error(
-        `Backend returned code ${error.status}, ` +
-        `body was: ${error.error}`);
-    }
-    // return an observable with a user-facing error message
-    return throwError(
-      'Something bad happened; please try again later.');
-  }
+  //   private handleError(error: HttpErrorResponse) {
+  //     if (error.error instanceof ErrorEvent) {
+  //       // A client-side or network error occurred. Handle it accordingly.
+  //       console.error('An error occurred:', error.error.message);
+  //     } else {
+  //       // The backend returned an unsuccessful response code.
+  //       // The response body may contain clues as to what went wrong,
+  //       console.error(
+  //         `Backend returned code ${error.status}, ` +
+  //         `body was: ${error.error}`);
+  //     }
+  //     // return an observable with a user-facing error message
+  //     return throwError(
+  //       'Something bad happened; please try again later.');
+  //   }
 
-}
+  // }
 
-export interface IFollower {
-  login: string;
-  id: number;
-  node_id: string;
-  avatar_url: string;
-  gravatar_id: string;
-  url: string;
-  html_url: string;
-  followers_url: string;
-  following_url: string;
-  gists_url: string;
-  starred_url: string;
-  subscriptions_url: string;
-  organizations_url: string;
-  repos_url: string;
-  events_url: string;
-  received_events_url: string;
-  type: string;
-  site_admin: boolean;
+  // export interface IFollower {
+  //   login: string;
+  //   id: number;
+  //   node_id: string;
+  //   avatar_url: string;
+  //   gravatar_id: string;
+  //   url: string;
+  //   html_url: string;
+  //   followers_url: string;
+  //   following_url: string;
+  //   gists_url: string;
+  //   starred_url: string;
+  //   subscriptions_url: string;
+  //   organizations_url: string;
+  //   repos_url: string;
+  //   events_url: string;
+  //   received_events_url: string;
+  //   type: string;
+  //   site_admin: boolean;
+  // }
+
 }
--- v2/portfolio/src/app/app-routing.module.ts
+++ v2/portfolio/src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AboutComponent } from './about/about.component';
 import { AppComponent } from './app.component';
 import { ExperienceComponent } from './experience/experience.component';
 import { FeaturedComponent } from './featured/featured.component';
